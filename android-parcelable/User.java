a3de64ddb4ae677bf4b7dc0d15c40be119317260
eranga herath <erangaeb@gmail.com>
2014-07-26 21:43:55 +0530

    added parcelable user

---
+++ android-parcelable/User.java
@@ -0,0 +1,126 @@
+package com.score.senzors.pojos;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Plain object to hold user attributes
+ * Need to implement parcelable, since Sensor object using User
+ * objects(Sensor is a parcelable object)
+ *
+ * @author erangaeb@gmail.com (eranga herath)
+ */
+public class User implements Parcelable {
+    String id;
+    String username;
+    String email;
+    String password;
+
+    public User(String id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    /**
+     * Use when reconstructing User object from parcel
+     * This will be used only by the 'CREATOR'
+     * @param in a parcel to read this object
+     */
+    public User(Parcel in) {
+        this.id = in.readString();
+        this.username = in.readString();
+        this.email = in.readString();
+        this.password = in.readString();
+    }
+
+    /**
+     * Define the kind of object that you gonna parcel,
+     * You can use hashCode() here
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * Actual object serialization happens here, Write object content
+     * to parcel one by one, reading should be done according to this write order
+     * @param dest parcel
+     * @param flags Additional flags about how the object should be written
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(username);
+        dest.writeString(email);
+        dest.writeString(password);
+    }
+
+    /**
+     * This field is needed for Android to be able to
+     * create new objects, individually or as arrays
+     *
+     * If you don’t do that, Android framework will through exception
+     * Parcelable protocol requires a Parcelable.Creator object called CREATOR
+     */
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof User) {
+            User toCompare = (User) obj;
+            return (this.username.equalsIgnoreCase(toCompare.getUsername()));
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return (this.getUsername()).hashCode();
+    }
+
+}
