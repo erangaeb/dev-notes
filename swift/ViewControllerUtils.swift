e943277a3aefb15d48a4b9a18b303587a3804726
eranga herath <erangaeb@gmail.com>
2014-08-14 12:17:47 +0530

    refactored swift utility functions

--- swift/ViewControllerUtils.swift
+++ swift/ViewControllerUtils.swift
@@ -7,32 +7,51 @@ class ViewControllerUtils {
     var loadingView: UIView = UIView()
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
 
-    func showActivityIndicatory(uiView: UIView) {
+    /*
+        Show customized activity indicator,
+        actually add activity indicator to passing view
+    
+        @param uiView - add activity indicator to this view
+    */
+    func showActivityIndicator(uiView: UIView) {
         container.frame = uiView.frame
         container.center = uiView.center
-        container.backgroundColor = uicolorFromHex(0xffffff, alpha: 0.3)
+        container.backgroundColor = UIColorFromHex(0xffffff, alpha: 0.3)
     
         loadingView.frame = CGRectMake(0, 0, 80, 80)
         loadingView.center = uiView.center
-        loadingView.backgroundColor = uicolorFromHex(0x444444, alpha: 0.7)
+        loadingView.backgroundColor = UIColorFromHex(0x444444, alpha: 0.7)
         loadingView.clipsToBounds = true
         loadingView.layer.cornerRadius = 10
     
         activityIndicator.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
         activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.WhiteLarge
         activityIndicator.center = CGPointMake(loadingView.frame.size.width / 2, loadingView.frame.size.height / 2);
+
         loadingView.addSubview(activityIndicator)
         container.addSubview(loadingView)
         uiView.addSubview(container)
         activityIndicator.startAnimating()
     }
 
+    /*
+        Hide activity indicator
+        Actually remove activity indicator from its super view
+    
+        @param uiView - remove activity indicator from this view
+    */
     func hideActivityIndicator(uiView: UIView) {
         activityIndicator.stopAnimating()
         container.removeFromSuperview()
     }
 
-    func uicolorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
+    /*
+        Define UIColor from hex value
+        
+        @param rgbValue - hex color value
+        @param alpha - transparency level
+    */
+    func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
         let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
         let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
         let blue = CGFloat(rgbValue & 0xFF)/256.0
@@ -42,8 +61,8 @@ class ViewControllerUtils {
 }
 
 //// In order to show the activity indicator, call the function from your view controller
-//// ViewControllerUtils().showActivityIndicatory(self.view)
+//// ViewControllerUtils().showActivityIndicator(self.view)
 
 //// In order to hide the activity indicator, call the function from your view controller
-//// ViewControllerUtils().hideActivityIndicatory(self.view)
+//// ViewControllerUtils().hideActivityIndicator(self.view)
 
