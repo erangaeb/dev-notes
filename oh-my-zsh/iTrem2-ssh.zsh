c814cccdce387115fdb9566c2f49249a5686eeb5
eranga herath <erangaeb@gmail.com>
2015-02-03 20:50:15 +0530

    cutom oh-my-zsh command to change iTerm2 profile when ssh'ing

---
+++ oh-my-zsh/iTrem2-ssh.zsh
@@ -0,0 +1,28 @@
+function tabc() {
+  NAME=$1; if [ -z "$NAME" ]; then NAME="Default"; fi 
+  # if you have trouble with this, change
+  # "Default" to the name of your default theme
+  echo -e "\033]50;SetProfile=$NAME\a"
+}
+
+function tab-reset() {
+    NAME="Default"
+    echo -e "\033]50;SetProfile=$NAME\a"
+}
+
+function colorssh() {
+    if [[ -n "$ITERM_SESSION_ID" ]]; then
+        trap "tab-reset" INT EXIT
+        if [[ "$*" =~ "production*" ]]; then
+            tabc Production
+        elif [[ "$*" =~ "staging*" ]]; then
+            tabc Staging 
+        else
+            tabc Other
+        fi
+    fi
+    ssh $*
+}
+compdef _ssh tabc=ssh
+
+alias ssh="colorssh"
